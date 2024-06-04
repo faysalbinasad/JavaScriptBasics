10092f9d9f3a31eda8f486ac34acc13f7d456ad5
Al-Faysal-Bin-Asad <you@example.com>
2024-06-05 03:17:02 +0600

    Sec: 23, V: 118, Up to: 32 mins

--- JSBasics-1.js
+++ JSBasics-1.js
@@ -1,1 +1,23 @@
 console.log("Hello, Bangladesh")
+
+// var/let/const
+
+let a = 4
+console.log(a)
+console.log(typeof(a))
+
+let b = 400.89
+console.log(typeof(b))
+
+let c = "Al Faysal Bin Asad"
+console.log(typeof(c))
+
+let required = true
+console.log(typeof(required))
+
+// let c = a+b (We can not redeclare 'let', but possible by 'var')
+
+c = a+b //Re-assigning is allowed in 'let'
+console.log(c)
+
+console.log(!required)
---
+++ JSBasics-2.js
@@ -0,0 +1,26 @@
+const flag = true
+
+if(!flag)
+{
+    console.log("Condition is satisfied")
+
+}
+else
+{
+    console.log(flag)
+    console.log("Condition is not satisfied")
+}    
+
+let i = 0
+while(i>10)
+{
+    i++
+    console.log(i)
+}
+
+do
+{
+
+    i++
+}while(i>10);
+console.log(i)
